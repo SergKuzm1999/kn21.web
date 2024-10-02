bf255e55467407b44e8a2a96de05bbe88271b118
SergKuzm1999 <sergiy.kuzmich1999@gmail.com>
2024-10-03 01:44:34 +0300

    completed 2 tasks out of 8

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+#Завдання № 1
+v_1 = 1
+v_2 = 1.23242424242424
+v_3 = "Hello World"
+v_4 = True
+print()
+print("Завдання № 1. Оголошення змінних та типи даних.")
+print()
+print(v_1, type(v_1))
+print(v_2, type(v_2))
+print(v_3, type(v_3))
+print(v_4, type(v_4))
+print("")
+
+#завдання №2
+v_1 = 5
+v_2 = 12
+print()
+print("Завдання № 2. Операції з числами.")
+print()
+print("v_1 =", v_1, "v_2 =", v_2)
+print("+", v_1 + v_2)
+print("-", v_1 - v_2)
+print("/", v_1 / v_2)
+print("*", v_1 * v_2)
+print("v_1 в 2 степені", pow(v_1,2))
